d9d59bd8da23028bca1af299a5e03b5bf13f4c8e
Manikya Chowdary <96164952+Manikya-1@users.noreply.github.com>
2023-11-16 10:17:36 +0530

    Add files via upload

--- head.php
+++ head.php
@@ -1,23 +1,46 @@
 <head>
-    <?php include 'pages/assets.php'; ?>
-    <?php include '../server/api.php'; ?>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard - Royal Express</title>
+<?php 
+    include 'server/api.php';  
+    include 'pages/assets.php';  
 
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    $setting = getAllSettings();
+    $res = mysqli_fetch_assoc($setting);
 
-    <link rel="stylesheet" href="assets/vendors/iconly/bold.css">
+    $header = $res['header_image'];
+    $header_src = "server/uploads/settings/".$header;
 
-    <link rel="stylesheet" href="assets/vendors/perfect-scrollbar/perfect-scrollbar.css">
-    <link rel="stylesheet" href="assets/vendors/bootstrap-icons/bootstrap-icons.css">
-    <link rel="stylesheet" href="assets/css/app.css">
-    <link rel="shortcut icon" href="assets/images/favicon.jpg" type="image/x-icon">
-    <style>
-        body {
-            background-color: #ffffff;
-        }
-    </style>
-</head>
+    $subheader = $res['sub_image'];
+    $subheader_src = "server/uploads/settings/".$subheader;
+
+    $about = $res['about_image'];
+    $about_src = "server/uploads/settings/".$about;
+
+    $background_image = $res['background_image'];
+    $background_image_src = "server/uploads/settings/".$background_image;
+
+
+    ?>
+    <title>Royal Express</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,700,900|Display+Playfair:200,300,400,700"> 
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/style.css">
+    
+  </head>
